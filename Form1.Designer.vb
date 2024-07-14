582ac80d4db4e3ad61e0f92cc7c7c3b50559ea61
Dominic-bii <dbii8830@gmail.com>
2024-07-14 20:58:07 +0300

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Add = New System.Windows.Forms.Button()
+        Me.Update = New System.Windows.Forms.Button()
+        Me.Delete = New System.Windows.Forms.Button()
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Title"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Author"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 139)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(120, 18)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Year Published"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 174)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 18)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Genre"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(153, 65)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(153, 101)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(153, 139)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(153, 174)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Add
+        '
+        Me.Add.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Add.Location = New System.Drawing.Point(15, 246)
+        Me.Add.Name = "Add"
+        Me.Add.Size = New System.Drawing.Size(81, 40)
+        Me.Add.TabIndex = 8
+        Me.Add.Text = "Add"
+        Me.Add.UseVisualStyleBackColor = True
+        '
+        'Update
+        '
+        Me.Update.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Update.Location = New System.Drawing.Point(15, 311)
+        Me.Update.Name = "Update"
+        Me.Update.Size = New System.Drawing.Size(81, 40)
+        Me.Update.TabIndex = 9
+        Me.Update.Text = "Update"
+        Me.Update.UseVisualStyleBackColor = True
+        '
+        'Delete
+        '
+        Me.Delete.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Delete.Location = New System.Drawing.Point(131, 246)
+        Me.Delete.Name = "Delete"
+        Me.Delete.Size = New System.Drawing.Size(81, 40)
+        Me.Delete.TabIndex = 10
+        Me.Delete.Text = "Delete"
+        Me.Delete.UseVisualStyleBackColor = True
+        '
+        'Clear
+        '
+        Me.Clear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Clear.Location = New System.Drawing.Point(131, 311)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(81, 40)
+        Me.Clear.TabIndex = 11
+        Me.Clear.Text = "Clear"
+        Me.Clear.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToOrderColumns = True
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(259, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(794, 575)
+        Me.DataGridView1.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1065, 599)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.Delete)
+        Me.Controls.Add(Me.Update)
+        Me.Controls.Add(Me.Add)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Add As Button
+    Friend WithEvents Update As Button
+    Friend WithEvents Delete As Button
+    Friend WithEvents Clear As Button
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Clear.Click
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,108 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Private connectionString As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Hp\source\repos\LibraryManagementSystem\LibraryManagementSystem\LibraryDB.mdf;Integrated Security=True"
+    Private connection As New SqlConnection(connectionString)
+    Private command As New SqlCommand
+    Private adapter As New SqlDataAdapter
+    Private dataTable As New DataTable
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Load book records into DataGridView
+        LoadBooks()
+    End Sub
+
+    Private Sub LoadBooks()
+        dataTable.Clear() ' Clear the dataTable
+        ExecuteQuery("SELECT * FROM Books", CommandType.Text)
+        DataGridView1.DataSource = dataTable
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles Add.Click
+        ' Add new book record
+        Dim titleParam As New SqlParameter("@Title", TextBox1.Text)
+        Dim authorParam As New SqlParameter("@Author", TextBox2.Text)
+        Dim yearPublishedParam As New SqlParameter("@YearPublished", TextBox3.Text)
+        Dim genreParam As New SqlParameter("@Genre", TextBox4.Text)
+
+        ExecuteQuery("INSERT INTO Books (Title, Author, YearPublished, Genre) VALUES (@Title, @Author, @YearPublished, @Genre)", CommandType.Text,
+                     titleParam,
+                     authorParam,
+                     yearPublishedParam,
+                     genreParam)
+        LoadBooks()
+        ClearFields()
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles Update.Click
+        ' Update existing book record
+        If DataGridView1.SelectedRows.Count > 0 Then
+            ' Populate TextBox controls with selected row's values
+            TextBox1.Text = DataGridView1.SelectedRows(0).Cells(1).Value.ToString()
+            TextBox2.Text = DataGridView1.SelectedRows(0).Cells(2).Value.ToString()
+            TextBox3.Text = DataGridView1.SelectedRows(0).Cells(3).Value.ToString()
+            TextBox4.Text = DataGridView1.SelectedRows(0).Cells(4).Value.ToString()
+
+            Dim idParam As New SqlParameter("@ID", DataGridView1.SelectedRows(0).Cells(0).Value)
+            Dim titleParam As New SqlParameter("@Title", TextBox1.Text)
+            Dim authorParam As New SqlParameter("@Author", TextBox2.Text)
+            Dim yearPublishedParam As New SqlParameter("@YearPublished", TextBox3.Text)
+            Dim genreParam As New SqlParameter("@Genre", TextBox4.Text)
+
+            ExecuteQuery("UPDATE Books SET Title = @Title, Author = @Author, YearPublished = @YearPublished, Genre = @Genre WHERE ID = @ID", CommandType.Text,
+                         idParam,
+                         titleParam,
+                         authorParam,
+                         yearPublishedParam,
+                         genreParam)
+            LoadBooks()
+            ClearFields()
+        Else
+            MessageBox.Show("Please select a row to update.")
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles Delete.Click
+        ' Delete book record
+        Dim idParam As New SqlParameter("@ID", DataGridView1.SelectedRows(0).Cells(0).Value)
+
+        ExecuteQuery("DELETE FROM Books WHERE ID = @ID", CommandType.Text,
+                     idParam)
+        LoadBooks()
+        ClearFields()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        ' Clear fields
+        ClearFields()
+    End Sub
+
+    Private Sub ClearFields()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+    End Sub
+
+    Private Sub ExecuteQuery(query As String, commandType As CommandType, ParamArray parameters As SqlParameter())
+        Try
+            connection.Open()
+            command.CommandText = query
+            command.Connection = connection
+            command.CommandType = commandType
+            For Each parameter As SqlParameter In parameters
+                command.Parameters.Add(parameter)
+            Next
+            If commandType = CommandType.Text Then
+                adapter.SelectCommand = command
+                adapter.Fill(dataTable)
+            Else
+                command.ExecuteNonQuery()
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+End Class
